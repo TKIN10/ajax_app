93c76a37dc6dc75a0908897fc3941d2b9ff7e725
TKIN10 <ace.1031.3d2y@icloud.com>
2023-01-06 15:08:50 +0900

    memo.jsの修正

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -4,6 +4,7 @@ function post (){
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
